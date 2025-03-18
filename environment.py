a8436fd7565f7e1ddc1eeba95bfd03142fb23258
chengq220 <chengqiu220@gmail.com>
2025-03-18 13:52:17 -0500

    connect the optimizer with the environment

--- environment.py
+++ environment.py
@@ -12,7 +12,7 @@ class environemnt():
     """
     A class that simulates the environment for the autonomous vehicle 
     """
-    def __init__(self, dim, timeStep = 1, numVehicles = 1, maxVel = 1):
+    def __init__(self, dim, numStep = 50, numVehicles = 1, maxVel = 1, radius = 25):
         """
         Initialize the environment
 
@@ -23,42 +23,35 @@ class environemnt():
             maxVel (int): The maximum velocity that the vehicle can achieve
         """
         self.dim = dim
-        self.step = timeStep
+        self.numStep = numStep
         self.maxVel = maxVel
-        self.screen = self.initDisplay(dim[0], dim[1])
-        self.road = self.__generateRoad()
+        self.radius = radius
+        self.dim = dim
         self.numVehicle = numVehicles
-        self.state, self.control = self.__generateVehicle(numVehicles)
+        self.road = self.__calcRoad()
+        self.vehicle = self.__generateVehicle()
+        self.obsState = self.__generateObstacle(numVehicles)
 
-    def initDisplay(self, width, height):
+    def __initDisplay(self, width, height):
         """
         Initialize the game screen
 
         Args:
             width (int): The width of the screen
             height (int): The height of the screen
-
-        Returns:
-            The screen object
         """
         pygame.init()
-        screen = pygame.display.set_mode((width, height))
-        return screen
+        self.screen = pygame.display.set_mode((width, height))
     
-    def __generateRoad(self):
+    def __calcRoad(self):
         """
-        Generate the road and return information about the boundar and the center
+        Return information about the boundar and the center
 
         Returns:
             A tuple containing the center and the boundary of the lane
         """
         roadCenter = self.dim[1] // 2
         roadWidth = self.dim[1] // 5
-        
-        # Setup the road
-        self.__drawDashedLine(ENV, stepsize=10)
-        pygame.draw.line(self.screen, ENV, (0, (roadCenter-roadWidth)), (self.dim[0], (roadCenter-roadWidth)), 1)
-        pygame.draw.line(self.screen, ENV, (0, (roadCenter+roadWidth)), (self.dim[0], (roadCenter+roadWidth)), 1)
         return (roadCenter, roadWidth)
 
     def __drawDashedLine(self, color, stepsize = 20):
@@ -74,7 +67,30 @@ class environemnt():
             if i%2 == 0:
                 pygame.draw.line(self.screen, color, (yVal[i],self.dim[1]//2), (yVal[i] + stepsize, self.dim[1]//2), 1)
 
-    def __generateVehicle(self, numVehicle):
+    def __generateVehicle(self):
+        """
+        Generate a vehicle that we are optimizing for
+
+        Returns:
+            Initial and end conditions 
+        """
+        maxSize = 50
+        # velocity, acceleration, angle, angular velocity
+        v = random.uniform(0.5,0.8) * random.uniform(0,1)
+        a = 0
+        theta = 0
+        w = 0
+        # x,y position
+        alpha = random.randint(0,1)
+        x = random.randint(0, self.dim[1]//8)
+        y = alpha * random.randint(self.road[0] + maxSize, self.road[0] + self.road[1] -maxSize)\
+            + (1-alpha) * random.randint(self.road[0]-self.road[1]+maxSize, self.road[0] - maxSize)
+        initial = np.array([x, y, v, theta, w, a])
+        end = np.array([self.road[0], y, v, theta, w, a])
+        # pygame.draw.circle(self.screen, PLAYER, (x,y), self.radius)
+        return (initial, end)
+
+    def __generateObstacle(self, numVehicle):
         """
         Generate the obstacle vehicles on the two side of the road
 
@@ -84,107 +100,58 @@ class environemnt():
         Returns:
             Dictionaries containing information about each vehicle generated
         """
-        # state = dict()
-        # control = dict()
-        # for i in range(numVehicle+1):
-        #     #generate the player vehicle:
-        #     if i == 0:
-        #         radius = 20
-        #     else: #Generate the obstacle vehicles
-        #         radius = 5 * random.randint(3,8)
-        #     maxSize = 50
-        #     # velocity, acceleration, angle, angular velocity
-        #     v = random.uniform(0.5,0.8) * random.uniform(0,1)
-        #     a = 0
-        #     theta = 0
-        #     w = 0
-            
-        #     # x,y position
-        #     alpha = random.randint(0,1)
-        #     x = random.randint(0, self.dim[1])
-        #     y = alpha * random.randint(self.road[0] + maxSize, self.road[0] + self.road[1] -maxSize)\
-        #         + (1-alpha) * random.randint(self.road[0]-self.road[1]+maxSize, self.road[0] - maxSize)
-    
-        #     curState = (x, y, theta, v, radius)
-        #     curControl = (w, a)
-        #     state[i] = curState
-        #     control[i] = curControl
-        #     pygame.draw.circle(self.screen, PLAYER if i == 0 else OBSTACLE, (x,y), radius)
-        # return state, control 
-        ###########################################################################################
-        # Testing Purposes
-        ###########################################################################################
-        state = dict() 
-        control = dict()
-        for i in range(numVehicle+1):
-            #generate the player vehicle:
+        obstacles = dict()
+        for i in range(numVehicle):
             maxSize = 50
-            radius = 10
+            # velocity, acceleration, angle, angular velocity
+            v = random.uniform(0.5,0.8) * random.uniform(0,1)
             a = 0
             theta = 0
             w = 0
-            if i == 0:
-                radius = 20
-                x = 0
-                y = self.road[0]
-                v = 0.1
-            else: #Generate the obstacle vehicles
-                radius = 20
-                # velocity, acceleration, angle, angular velocity
-                v = 0.05
-                # x,y position
-                x = 125
-                y = self.road[0]
+            # x,y position
+            alpha = random.randint(0,1)
+            x = random.randint(self.dim[1]//8 + 2*self.radius, self.dim[1] - self.dim[1]//4)
+            y = alpha * random.randint(self.road[0] + maxSize, self.road[0] + self.road[1] -maxSize)\
+                + (1-alpha) * random.randint(self.road[0]-self.road[1]+maxSize, self.road[0] - maxSize)
+            feature = np.array([x, y, v, theta, w, a])
+            # pygame.draw.circle(self.screen, OBSTACLE, (x,y), self.radius)
+            obstacles[i] = feature
+        return obstacles
 
-            curState = (x, y, theta, v, radius)
-            curControl = (w, a)
-            state[i] = curState
-            control[i] = curControl
-            pygame.draw.circle(self.screen, PLAYER if i == 0 else OBSTACLE, (x,y), radius)
-        return state, control 
-    
-    def __update(self):
+    def optimize(self):
         """
-        Update the new position of the vehicles
+        Solves the nonlinear optimization problem to find the best control vectors for the trajectory
         """
-        self.control[0] = self.__optimize()
-        for key in self.state.keys():
-            # update for the parameters according to formulas for velocity and angular velocity
-            x0,y0,theta0,v0,radius = self.state[key] 
-            a, w = self.control[key]
-            v = v0 + a * self.step
-            theta = theta0 + w * self.step
-            x = (x0 + v * np.cos(theta))
-            y = (y0 + v * np.sin(theta))
-            if(key != 0): #only make the obstacle circle back
-                x = x % self.dim[0]
-                y = y % self.dim[1] 
-            self.state[key] = (x,y, theta, v, radius)
+        radius = [self.radius] * (len(self.obsState.keys()) + 1)
+        bounds = [(0, self.dim[1]), (self.dim[0]//2 - self.dim[0]//5, self.dim[0]//2 + self.dim[0]//5), \
+                  (0, self.maxVel),(-np.pi/2, np.pi/2),(None, None),(None, None)]
+        model = AVP(self.vehicle, self.obsState, radius, bounds, numStep=self.numStep,maxIter=10)
+        sol = model.forward()
+        self.sol = (sol[0::6], sol[1::6])
+        self.obstacleTrajectory = model.obstacles
 
-    def __optimize(self):
-        """
-        Solves the nonlinear optimization problem to find the best 
-        control vector for a given time stamp
-        """
-        normal = np.array([[1, 0], [-1, 0]]) # only for horizontal stright lines
-        center, diff = self.road
-        offsets = ((center-diff), (center + diff))
-        mod = AVP(var=len(self.control[0]), con = 3, A = normal, C = offsets, state = self.state, \
-                  control = self.control) 
-        guess = np.array([3, 4, 0, 0.5, 10, 0]) 
-        sol, _ = mod.solve(guess)
-        self.control[0] = sol
-
-    def __refreshFrame(self):
+    def __refreshFrame(self, timstep):
         """
         Refreshes the screen with updated values
         """
         self.screen.fill(SCREEN) 
-        self.__generateRoad()
-        self.__update()
-        for key in self.state.keys():
-            x,y,_,_, radius = self.state[key]
-            pygame.draw.circle(self.screen, PLAYER if key == 0 else OBSTACLE, (x,y), radius)
+
+        # Update the desire vehicle
+        vx, vy = self.sol
+        pygame.draw.circle(self.screen, PLAYER, (vx[timstep],vy[timstep]), self.radius)
+        
+        # Draw the road for each frame
+        self.__drawDashedLine(ENV, stepsize=10)
+        roadCenter, roadWidth = self.road
+        pygame.draw.line(self.screen, ENV, (0, (roadCenter-roadWidth)), (self.dim[0], (roadCenter-roadWidth)), 1)
+        pygame.draw.line(self.screen, ENV, (0, (roadCenter+roadWidth)), (self.dim[0], (roadCenter+roadWidth)), 1)
+
+        # Update the obstacles 
+        for idx in range(self.obstacleTrajectory.shape[0]):
+            obs_x = self.obstacleTrajectory[idx, 0::6][timstep]
+            obs_y = self.obstacleTrajectory[idx, 1::6][timstep]
+            pygame.draw.circle(self.screen, OBSTACLE, (obs_x,obs_y), self.radius)
+        
         pygame.display.update()
 
     def runGame(self):
@@ -192,9 +159,13 @@ class environemnt():
         Run the simulation/game
         """
         running = True
-        while running and self.state[0][0] <= self.dim[0]:
+        timestep = 0
+        self.__initDisplay(self.dim[0], self.dim[1])
+        while running and timestep < self.numStep:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            self.__refreshFrame()
+            self.__refreshFrame(timestep)
+            timestep = timestep + 1
+            pygame.time.delay(1000)
         pygame.quit()
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from environment import environemnt
 import numpy as np
 
-def startSimulation(dim, timeStep = 1, numVehicles = 1, maxVelocity = 2):
-    env = environemnt([dim, dim], timeStep = timeStep, maxVel = maxVelocity)
+def startSimulation(dim, numSteps = 50, numVehicles = 1, maxVelocity = 2):
+    """
+    Starts the simulation
+    """
+    env = environemnt([dim, dim], numStep = numSteps,numVehicles=numVehicles, maxVel = maxVelocity)
+    env.optimize()
     env.runGame()
 
 if __name__ == "__main__":
-    startSimulation(580)
+    startSimulation(580, numVehicles= 3)
